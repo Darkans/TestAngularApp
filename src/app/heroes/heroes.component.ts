fb0046157497e7c2b4ded623ff29d30204f363b1
bychkovs-1 <Vadim.Bychkovskiy@mt.com>
2019-02-06 17:06:24 +0300

    Initial

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Models/Hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroesList: Hero[];
+  refreshHeroes = () => {
+    this.heroesList = this.getHeroList(5);
+  }
+
+  constructor() {
+    this.heroesList = this.getHeroList(5);
+  }
+
+  ngOnInit() {
+  }
+
+  private getHeroList(count:number): Hero[] {
+    let result: Hero[] = [];
+
+    for (let i = 0; i < count; i++) {
+      let newHero = new Hero(i + 1, `heroname_${i + 1}`);
+      result.push(newHero);
+    }
+
+    return result;
+  }
+}
